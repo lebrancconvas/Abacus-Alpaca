3248fd7075f6e754c338ed2649e594808c7b1e12
lebrancconvas <lebrancconvas@gmail.com>
2022-09-21 18:21:35 +0700

    ✨ feat: create raylib-go window

--- app/main.go
+++ app/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"github.com/gen2brain/raylib-go/raylib"
+)
 
+const (
+	screenWidth, screenHeight = 640, 480;
+	titleName = "Abacus Alpaca";
+	fps = 60;
 )
 
 func main() {
-	
+	rl.InitWindow(screenWidth, screenHeight, titleName);
+	rl.SetTargetFPS(fps); 
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing();
+		rl.ClearBackground(rl.RayWhite);
+		rl.DrawText("Alpaca", 100, 100, 20, rl.Black);
+		rl.EndDrawing();
+	}
+
+	rl.CloseWindow();
 }
