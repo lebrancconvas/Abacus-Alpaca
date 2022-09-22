0adedd77bec4926cdf00fbc8962ab5bc3dbf8a17
lebrancconvas <lebrancconvas@gmail.com>
2022-09-22 17:52:38 +0700

    ✨ feat: update

--- app/main.go
+++ app/main.go
@@ -10,20 +10,56 @@ const (
 	fps = 60;
 )
 
-func main() {
+var (
+	bgColor = rl.NewColor(255, 196, 196, 0);
+)
+
+// Init. 
+func init() {
 	rl.InitWindow(screenWidth, screenHeight, titleName);
+	rl.SetExitKey(0);
 	rl.SetTargetFPS(fps); 
+}
 
+// Input. 
+func input() {
+
+}
+
+// Game Loop. 
+func update() {
+
+}
+
+// Draw. 
+func drawScene() {
+	rl.DrawText("Alpaca", 100, 100, 20, rl.Black);
+}
+
+// Rendering. 
+func render() {
+			// Begin Draw 
+			rl.BeginDrawing();
+
+			rl.ClearBackground(bgColor); // Background Color. 
+			drawScene();
+	
+			// End Draw 
+			rl.EndDrawing();
+}
+
+// Quit. 
+func quit() {
+	rl.CloseWindow();
+}
+
+// Main Function. 
+func main() {
 	for !rl.WindowShouldClose() {
-		// Begin Draw 
-		rl.BeginDrawing();
-
-		rl.ClearBackground(rl.Green); // Background Color. 
-		rl.DrawText("Alpaca", 100, 100, 20, rl.Black);
-
-		// End Draw 
-		rl.EndDrawing();
+		input();
+		update();
+		render();
 	}
 
-	rl.CloseWindow();
+	quit();
 }
