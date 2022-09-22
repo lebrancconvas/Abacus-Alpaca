d4530106102ed243b119adfe7eb2b30b4434c784
lebrancconvas <lebrancconvas@gmail.com>
2022-09-22 18:58:20 +0700

    ✨ feat: add player to screen + input

--- app/main.go
+++ app/main.go
@@ -13,7 +13,13 @@ const (
 var (
 	running = true; 
 	bgColor = rl.NewColor(255, 196, 196, 0);
+
 	grassTile rl.Texture2D; 
+	playerSprite rl.Texture2D; 
+	playerSrc rl.Rectangle;
+	playerDest rl.Rectangle;
+
+	playerSpeed float32 = 3;
 )
 
 // Init. 
@@ -23,11 +29,26 @@ func init() {
 	rl.SetTargetFPS(fps); 
 
 	grassTile = rl.LoadTexture("../assets/pack/Sprout_Lands/Tilesets/Grass.png");
+	playerSprite = rl.LoadTexture("../assets/pack/Toon_Character/Robot/Tilesheet/character_robot_sheet.png"); 
+
+	playerSrc = rl.NewRectangle(0, 0, 100, 160);
+	playerDest = rl.NewRectangle(80, 100, 100, 100);
 }
 
 // Input. 
 func input() {
-
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerDest.Y -= playerSpeed;
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerDest.Y += playerSpeed;
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerDest.X -= playerSpeed;
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerDest.X += playerSpeed;
+	}
 }
 
 // Game Loop. 
@@ -38,6 +59,7 @@ func update() {
 // Draw. 
 func drawScene() {
 	rl.DrawTexture(grassTile, 100, 100, rl.White);
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White); 
 }
 
 // Rendering. 
@@ -55,6 +77,9 @@ func render() {
 // Quit. 
 func quit() {
 	rl.UnloadTexture(grassTile); 
+
+	rl.UnloadTexture(playerSprite);  
+
 	rl.CloseWindow();
 }
 
